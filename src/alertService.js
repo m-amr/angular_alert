e2d2e368534b9d08212efd081dd1be57bced5b1b
mohamed amr <m_ma7med@hotmail.com>
2015-11-14 17:09:15 +0200

    create angular alertService

---
+++ src/alertService.js
@@ -0,0 +1,34 @@
+
+alertModule.service('alertService', ['$rootScope', function($rootScope){
+
+    var _service = this;
+    var _alertEventName = 'event:alert';
+    var _alertInfoEventName = _alertEventName +':info';
+    var _alertWarningEventName = _alertEventName +':warning';
+    var _alertErrorEventName = _alertEventName +':error';
+
+    _service.getInfoEvent = function(){
+        return _alertInfoEventName;
+    };
+
+    _service.getWarningEvent = function(){
+        return _alertWarningEventName;
+    };
+
+    _service.getErrorEvent = function(){
+        return _alertErrorEventName;
+    };
+
+    _service.info = function(message){
+        $rootScope.$broadcast(_service.getInfoEvent(), message);
+    };
+
+    _service.warning = function(message){
+        $rootScope.$broadcast(_service.getWarningEvent(), message);
+    };
+
+    _service.error = function(message){
+        $rootScope.$broadcast(_service.getErrorEvent(), message);
+    };
+
+}]);
