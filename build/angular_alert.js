6e03b49904845205a3c9ca2b7adf39e5752aa1c8
mohamed amr <m_ma7med@hotmail.com>
2015-11-15 01:41:40 +0200

    interpolated template and pass message and className to the template

--- build/angular_alert.js
+++ build/angular_alert.js
@@ -1,4 +1,4 @@
-/*! angular_alert - v1.0.0 - 2015-11-14 */ 
+/*! angular_alert - v1.0.0 - 2015-11-15 */ 
 (function(){
 
 if(typeof angular === 'undefined'){
@@ -40,23 +40,46 @@ alertModule.service('alertService', ['$rootScope', function($rootScope){
     };
 
 }]);
-
-alertModule.directive('alertComponent', ['alertService', function(alertService){
+alertModule.directive('alertComponent', ['alertService', '$compile', '$interpolate',  function(alertService, $compile, $interpolate){
 
     var _template =
-        '<div class="absolute alert alert-warning alert-dismissible" role="alert">' +
+        '<div class="absolute alert alert-{{className}} alert-dismissible" role="alert">' +
         '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
-        '<strong>Warning!</strong> Better check yourself,  not looking too good.'+
+        '{{message}}'+
         '</div>';
     var _toggle = true;
+    var _infoClassName = 'info';
+    var _warningClassName = 'warning';
+    var _errorClassName = 'danger';
+
+    var getAlertClassNameBasedOnEvent = function(eventName){
+        switch (eventName){
+            case alertService.getWarningEvent():
+                return _warningClassName;
+            case alertService.getInfoEvent():
+                return _infoClassName;
+            case alertService.getErrorEvent():
+                return _errorClassName;
+
+            default:
+                throw eventName+' is not defined.';
+        }
+
+
+    };
 
     return {
         restricted : 'A',
         link : function(scope, element, attrs){
-            console.log('Linking directive');
-            scope.$on(alertService.getWarningEvent(), function(){
+            scope.$on(alertService.getWarningEvent(), function($event, message){
+                var _className = getAlertClassNameBasedOnEvent(alertService.getWarningEvent());
                 if(_toggle){
-                    element.html(_template);
+                    var model = {
+                        'message': message,
+                        'className': _className
+                    };
+                    var _interpolatedTemplate = $interpolate(_template)(model);
+                    element.html(_interpolatedTemplate);
                     _toggle = false;
                 }else{
                     element.html('');
@@ -67,6 +90,7 @@ alertModule.directive('alertComponent', ['alertService', function(alertService){
         }
     };
 }]);
+
 
 if(typeof define === 'function'){
     define([], function(){
--- src/alertDirective.js
+++ src/alertDirective.js
@@ -1,20 +1,43 @@
-
-alertModule.directive('alertComponent', ['alertService', function(alertService){
+alertModule.directive('alertComponent', ['alertService', '$compile', '$interpolate',  function(alertService, $compile, $interpolate){
 
     var _template =
-        '<div class="absolute alert alert-warning alert-dismissible" role="alert">' +
+        '<div class="absolute alert alert-{{className}} alert-dismissible" role="alert">' +
         '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
-        '<strong>Warning!</strong> Better check yourself,  not looking too good.'+
+        '{{message}}'+
         '</div>';
     var _toggle = true;
+    var _infoClassName = 'info';
+    var _warningClassName = 'warning';
+    var _errorClassName = 'danger';
+
+    var getAlertClassNameBasedOnEvent = function(eventName){
+        switch (eventName){
+            case alertService.getWarningEvent():
+                return _warningClassName;
+            case alertService.getInfoEvent():
+                return _infoClassName;
+            case alertService.getErrorEvent():
+                return _errorClassName;
+
+            default:
+                throw eventName+' is not defined.';
+        }
+
+
+    };
 
     return {
         restricted : 'A',
         link : function(scope, element, attrs){
-            console.log('Linking directive');
-            scope.$on(alertService.getWarningEvent(), function(){
+            scope.$on(alertService.getWarningEvent(), function($event, message){
+                var _className = getAlertClassNameBasedOnEvent(alertService.getWarningEvent());
                 if(_toggle){
-                    element.html(_template);
+                    var model = {
+                        'message': message,
+                        'className': _className
+                    };
+                    var _interpolatedTemplate = $interpolate(_template)(model);
+                    element.html(_interpolatedTemplate);
                     _toggle = false;
                 }else{
                     element.html('');
