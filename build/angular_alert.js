258155a0326103682bc77675b034178a5ca7ce19
mohamed amr <m_ma7med@hotmail.com>
2015-11-15 01:47:41 +0200

    handle alert info and alert error events

--- build/angular_alert.js
+++ build/angular_alert.js
@@ -47,7 +47,7 @@ alertModule.directive('alertComponent', ['alertService', '$compile', '$interpola
         '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
         '{{message}}'+
         '</div>';
-    var _toggle = true;
+
     var _infoClassName = 'info';
     var _warningClassName = 'warning';
     var _errorClassName = 'danger';
@@ -68,23 +68,29 @@ alertModule.directive('alertComponent', ['alertService', '$compile', '$interpola
 
     };
 
+    var getModelData = function(message, evenName){
+        return {
+            'message' : message,
+            'className': getAlertClassNameBasedOnEvent(evenName)
+        }
+    };
+
     return {
         restricted : 'A',
         link : function(scope, element, attrs){
             scope.$on(alertService.getWarningEvent(), function($event, message){
-                var _className = getAlertClassNameBasedOnEvent(alertService.getWarningEvent());
-                if(_toggle){
-                    var model = {
-                        'message': message,
-                        'className': _className
-                    };
-                    var _interpolatedTemplate = $interpolate(_template)(model);
-                    element.html(_interpolatedTemplate);
-                    _toggle = false;
-                }else{
-                    element.html('');
-                    _toggle = true;
-                }
+                var _interpolatedTemplate = $interpolate(_template)(getModelData(message, alertService.getWarningEvent()));
+                element.html(_interpolatedTemplate);
+            });
+
+            scope.$on(alertService.getInfoEvent(), function($event, message){
+                var _interpolatedTemplate = $interpolate(_template)(getModelData(message, alertService.getInfoEvent()));
+                element.html(_interpolatedTemplate);
+            });
+
+            scope.$on(alertService.getErrorEvent(), function($event, message){
+                var _interpolatedTemplate = $interpolate(_template)(getModelData(message, alertService.getErrorEvent()));
+                element.html(_interpolatedTemplate);
             });
 
         }
