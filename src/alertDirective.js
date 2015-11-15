1d80573c09a35d85ed7cf254c53b29eea7aadc18
mohamed amr <m_ma7med@hotmail.com>
2015-11-15 02:01:19 +0200

    remove alert element content when close button is clicked.

--- src/alertDirective.js
+++ src/alertDirective.js
@@ -2,7 +2,7 @@ alertModule.directive('alertComponent', ['alertService', '$compile', '$interpola
 
     var _template =
         '<div class="absolute alert alert-{{className}} alert-dismissible" role="alert">' +
-        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true" ng-click="closeClicked()">&times;</span></button>' +
         '{{message}}'+
         '</div>';
 
@@ -36,19 +36,27 @@ alertModule.directive('alertComponent', ['alertService', '$compile', '$interpola
     return {
         restricted : 'A',
         link : function(scope, element, attrs){
+            scope.closeClicked = function(){
+                console.log('close is clicked..');
+                element.html('');
+            };
+
             scope.$on(alertService.getWarningEvent(), function($event, message){
                 var _interpolatedTemplate = $interpolate(_template)(getModelData(message, alertService.getWarningEvent()));
-                element.html(_interpolatedTemplate);
+                var _compiledTemplate = $compile(_interpolatedTemplate)(scope);
+                element.append(_compiledTemplate)
             });
 
             scope.$on(alertService.getInfoEvent(), function($event, message){
                 var _interpolatedTemplate = $interpolate(_template)(getModelData(message, alertService.getInfoEvent()));
-                element.html(_interpolatedTemplate);
+                var _compiledTemplate = $compile(_interpolatedTemplate)(scope);
+                element.append(_compiledTemplate)
             });
 
             scope.$on(alertService.getErrorEvent(), function($event, message){
                 var _interpolatedTemplate = $interpolate(_template)(getModelData(message, alertService.getErrorEvent()));
-                element.html(_interpolatedTemplate);
+                var _compiledTemplate = $compile(_interpolatedTemplate)(scope);
+                element.append(_compiledTemplate)
             });
 
         }
